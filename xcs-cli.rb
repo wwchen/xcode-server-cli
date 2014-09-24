1b50bd0347443f973898d7fc6a34a3225168f15c
William Chen <williamchen16@gmail.com>
2014-09-24 14:10:03 -0700

    Major overhaul for XCode 6. The code is much cleaner now

--- xcs-cli.rb
+++ xcs-cli.rb
@@ -1,30 +1,33 @@
 #!/usr/bin/env ruby
 
-require_relative 'xcs-base'
+require_relative 'xcs'
 
 #output = send(*ARGV)
 
 if ARGV.count < 2
-  puts "Usage: ./xcs-cli.rb (status|run) [bot name]"
+  puts "Usage: ./xcs-cli.rb (status|run|bot-config|cancel) [args]"
   puts "Regex is supported for bot name"
   exit
 end
 
 action = ARGV[0]
-bot_name = Regexp.new ARGV[1], Regexp::IGNORECASE
-bots = get_bots.keep_if { |b| b.longName =~ bot_name }
-
-#bots.each { |b| prints "%s %s\n", b.longName, b.guid }
 
 case action
 when "status"
-  bots.each do |bot|
-    bot = Bot.new bot
-    printf "%-30s %s\n", bot.name, bot.latest_run_status
-  end
+    XCodeInteractive.print_bots
+when "bot-config"
+    bot_name = ARGV[1]
+    bot_ids = XCodeInteractive.find_bot_ids_by_name bot_name
+    bot_ids.each do |bot_id|
+        XCodeInteractive.print_bot_configuration bot_id
+    end
 when "run"
-  bots.each do |bot|
-    bot = Bot.new bot
-    schedule_botrun bot.guid
-  end
+    bot_name = ARGV[1]
+    bot_ids = XCodeInteractive.find_bot_ids_by_name bot_name
+    bot_ids.each do |bot_id|
+        XCodeInteractive.integrate_bot bot_id
+    end
+when "cancel"
+    integration_id = ARGV[1]
+    interface.cancel_integration integration_id
 end
--- xcs.rb
+++ xcs.rb
@@ -1,64 +1,347 @@
 #!/usr/bin/env ruby
 
-require_relative 'xcs-base'
+require 'json'
+require 'net/http'
+require 'openssl'
+require 'ostruct'
+require 'yaml'
+require 'singleton'
 
-#output = send(*ARGV)
+DEBUG = false
+VERBOSE = true
+CHECKMARK = "✔"
+CROSSMARK = "✘"
+CACHE_EXPIRY = 5
 
-if ARGV.count < 2
-  puts "Usage: ./xcs-cli.rb (status|run|update-branch|cancel) [bot name] [additional args]"
-  puts "Regex is supported for bot name"
-  exit
+#####################################################################
+
+# Strip out special spaces (Alt+space)
+class String
+    def to_utf8
+        self.encode('UTF-8', { :invalid => :replace, :undef => :replace, :replace => '' })
+    end
 end
 
-action = ARGV[0]
-bot_name = Regexp.new ARGV[1], Regexp::IGNORECASE
-bots = get_bots.keep_if { |b| b.longName =~ bot_name }
-
-#bots.each { |b| prints "%s %s\n", b.longName, b.guid }
-
-case action
-when "status"
-  bots.each do |bot|
-    printf "%-30s %s\n", bot.name, bot.latest_run_status
-    bot.botruns.last.to_s
-  end
-when "run"
-  bots.each do |bot|
-    response = bot.integrate
-    printf "Scheduled botrun for %s\n", bot.name
-    printf "Integration queued is %s\n", response.integration
-  end
-when "create-test"
-  options = {
-    "name" => "test",
-    "buildProjectPath" => "common/AXPlatformTest/AXPlatformTest.xcodeproj",
-    "buildSchemeName" => "CI Build",
-    "scmBranch" => "main",
-    "scmGUID" => "2ded7b82-44b1-b7d0-4ca8-15a6a266c7f1"
-  }
-  printf "%s\n", Bot.new(options).to_h
-when "update-branch"
-    branch_name = ARGV[2]
-    bots.each do |bot|
-        options = { "branch" => branch_name }
-        bot.update options
+class Float
+    def to_date (format = "%a %b %d, %l:%M %p")
+        Time.at(self).strftime(format)
     end
-when "cancel"
-    # TODO wichen: I think it's a little buggy. Doens't always work
-    bot_names = bots.map { |b| b.name }
-    puts "Canceling #{bot_names} until script is interrupted\n"
-    while true
-        bots.each do |bot|
-            status = bot.latestRunStatus
-            if status =~ /(running|integrating)/
-                printf "%s is running. Canceling...\n", bot.name
-                bot.cancel
-            elsif status =~ /(completed|canceled)/
-            else
-                printf "%s: %s\n", bot.name, status
-            end
+end
+
+#####################################################################
+
+# to view all supported routes, see the Xcode Server source code:
+# /Applications/Xcode.app/Contents/Developer/usr/share/xcs/xcsd/routes/routes.js
+class XCodeAPI
+    @hostname = nil
+    @username = nil
+    @password = nil
+    @http = nil
+
+    def initialize (config_file = nil)
+        if config_file
+            #config = YAML.load_file(File.join(File.dirname(File.expand_path(__FILE__)),"config.yml")) unless defined? CONFIG
+            config = YAML.load_file config_file
+            set_hostname config["HOSTNAME"]
+            set_credentials config["USERNAME"], config["PASSWORD"]
+            puts "Config file loaded: #{config_file}" if DEBUG
+        else
+            puts "No config file provided" if DEBUG
+        end
+    end
+
+    def set_hostname (hostname)
+        @hostname = hostname.strip
+
+        @http = Net::HTTP.new(@hostname, 20343)
+        @http.use_ssl = true
+        @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+
+    def set_credentials (username, password)
+        @username = username.strip
+        @password = password.strip
+    end
+
+    def get_request (pathString, headers = {})
+        request = Net::HTTP::Get.new "/api/#{pathString}"
+        request.basic_auth @username, @password
+        return @http.request request
+    end
+
+    def post_request (pathString, data, headers = {})
+        request = Net::HTTP::Post.new "/api/#{pathString}"
+        request.basic_auth @username, @password
+        request.set_form_data data
+        return @http.request request
+    end
+end
+
+# TODO for a lack of a better class name..
+class XCodeAPIInterface
+    include Singleton
+    @api = nil
+
+    def initialize
+        config_filename = "config.yml"
+        config_file = File.join(File.dirname(File.expand_path(__FILE__)), config_filename)
+        @api = XCodeAPI.new config_file
+    end
+
+    def make_request (type, url, data = nil)
+        response = nil
+        if type == "get" || type == "GET"
+            response = @api.get_request url
+        elsif type == "post" || type == "PUT"
+            raise ArgumentError, "data cannot be nil" if data.nil?
+            response = @api.post_request url, data
         end
 
-        sleep 2
+        case response
+        when Net::HTTPSuccess, NET::HTTPOK, NET::HTTPCreated
+            return JSON.parse response.body.to_utf8
+        when nil
+            raise IOError, "#{type} request failed: response is nil for #{url}"
+        else
+            return response.value
+            #raise Net::HTTPError, response.value, response
+            #raise Net::HTTPError, "#{type} request failed: #{url}", response
+        end
+    end
+
+    # health and misc
+    def ping
+        return make_request "get", "ping"
+    end
+
+    def hostname
+        return make_request "get", "hostname"
+    end
+
+    def health
+        return make_request "get", "health"
+    end
+
+    # bots
+    def list_bots
+        return make_request "get", "bots"
+    end
+
+    def find_bot (id)
+        return make_request "get", "bots/#{id}"
+    end
+
+    def create_bot (data)
+        return make_request "post", "bots", data
+    end
+
+    def show_bot_stats (id)
+        return make_request "get", "bots/#{id}/stats"
+    end
+
+    # integrations
+    def create_integration_for_bot (bot_id, data)
+        return make_request "post", "bots/#{bot_id}/integrations", data
+    end
+
+    def integration_count_for_bot (bot_id)
+        return make_request "get", "bots/#{bot_id}/integrations/count"
+    end
+
+    def integrations_for_bot (bot_id)
+        return make_request "get", "bots/#{bot_id}/integrations"
+    end
+
+    def find_integration (id)
+        return make_request "get", "integrations/#{id}"
+    end
+
+    def cancel_integration (id)
+        return make_request "post", "integrations/#{id}/cancel"
     end
 end
+
+class XCodeInteractive
+    def self.print_bots
+        interface = XCodeAPIInterface.instance
+        response = interface.list_bots
+
+        count = response["count"]
+        results = response["results"]
+        printf "%-8s %-25s %-15s %-10s %s\n", "Type", "Name", "Scheme", "Tiny ID", "ID"
+        puts "=" * 94
+        results.each do |result|
+            printf("%-8s %-25s %-15s %-10s %s\n",
+                result["doc_type"],
+                result["name"],
+                result["configuration"]["schemeName"],
+                result["tinyID"],
+                result["_id"])
+        end
+        puts "Total bots: #{count}"
+    end
+
+    def self.print_bot_configuration (id)
+        interface = XCodeAPIInterface.instance
+        response = interface.find_bot id
+
+        puts JSON.pretty_generate response if DEBUG
+
+        puts response["name"]
+        puts "=" * 80
+        printf "%-20s: %s\n",                "id", response["_id"]
+        printf "%-20s: %s\n",           "tiny id", response["tinyID"]
+        printf "%-20s: %s\n",      "integrations", response["integration_counter"]
+        printf "%-20s: %s\n",            "scheme", response["configuration"]["schemeName"]
+        printf "%-20s: %s\n",       "clean build", response["configuration"]["buildFromClean"]
+        printf "%-20s: %s\n", "schedule interval", response["configuration"]["periodicScheduleInterval"]
+        printf "%-20s: %s\n",   "testing devices", response["configuration"]["testingDeviceIDs"].to_s
+        printf "%-20s: %s\n",     "schedule type", response["configuration"]["scheduleType"].to_s
+        printf "%-20s: %s\n",             "test?", response["configuration"]["performsTestAction"].to_s
+        printf "%-20s: %s\n",          "analyze?", response["configuration"]["performsAnalyzeAction"].to_s
+        printf "%-20s: %s\n",          "archive?", response["configuration"]["performsArchiveAction"].to_s
+        puts "-" * 80
+        puts "Source control info"
+        puts "-" * 80
+        repo_id = response["configuration"]["sourceControlBlueprint"]["DVTSourceControlWorkspaceBlueprintPrimaryRemoteRepositoryKey"]
+        printf "%-20s: %s\n",              "name", response["configuration"]["sourceControlBlueprint"]["DVTSourceControlWorkspaceBlueprintLocationsKey"][repo_id]["DVTSourceControlBranchIdentifierKey"]
+        printf "%-20s: %s\n",                "id", repo_id
+        printf "%-20s: %s\n",      "project path", response["configuration"]["sourceControlBlueprint"]["DVTSourceControlWorkspaceBlueprintRelativePathToProjectKey"]
+        printf "%-20s: %s\n", "working copy path", response["configuration"]["sourceControlBlueprint"]["DVTSourceControlWorkspaceBlueprintWorkingCopyPathsKey"][repo_id]
+        puts "-" * 80
+        puts "Triggers"
+        puts "-" * 80
+        triggers = response["configuration"]["triggers"]
+        triggers.each_with_index do |trigger, index|
+            printf "%-20s: %s\n",        "name", trigger["name"]
+            printf "%-20s: %s\n",        "type", trigger["type"]
+            printf "%-20s: %s\n",       "phase", trigger["phase"]
+            printf "%-20s: %s\n",  "conditions", trigger["conditions"].to_s
+            printf "%-20s: %s\n", "script body", trigger["scriptBody"].delete("\n") if trigger["scriptBody"] 
+            puts "-" * 40 if index % 2 == 1
+        end
+    end
+
+    def self.print_integrations_for_bot (id, count = nil)
+        interface = XCodeAPIInterface.instance
+        response = interface.integrations_for_bot id
+        puts JSON.pretty_generate reponse if DEBUG
+        count = response["count"]
+        results = response["results"]
+        if count > 1
+            name = results.first["bot"]["name"]
+            puts "Integrations for bot: #{name}"
+            puts "=" * 80
+            printf "%-8s %-25s %-15s %-10s %-10s %-10s %-10s %-16s %-8s %-8s %-12s %s\n", "Number", "Status", "Result", "Analyzer", "Warning", "Error", "Test", "Queued", "Start", "End", "Duration", "ID"
+            results.reverse.each do |result|
+                status = result["currentStep"]
+                printf("%-8s %-25s %-15s %-10s %-10s %-10s %-10s %-16s %-8s %-8s %-12s %s\n",
+                    result["number"],
+                    status,
+                    result["result"],
+                    result["buildResultSummary"]["analyzerWarningCount"],
+                    result["buildResultSummary"]["warningCount"],
+                    result["buildResultSummary"]["errorCount"],
+                    result["buildResultSummary"]["testsCount"],
+                    DateTime.parse(result["queuedDate"]).to_time.getlocal.strftime("%b %d, %R"),
+                    status =~ /pending/ ? "-" : DateTime.parse(result["startedTime"]).to_time.getlocal.strftime("%R"),
+                    status =~ /pending/ ? "-" : DateTime.parse(result["endedTime"]).to_time.getlocal.strftime("%R"),
+                    result["duration"],
+                    result["_id"])
+            end
+        end
+    end
+
+    def self.print_integration (id)
+        interface = XCodeAPIInterface.instance
+        response = interface.find_integration id
+        puts JSON.pretty_generate response if DEBUG
+
+        bundle_name = response["assets"]["product"]["infoDictionary"]["CFBundleExecutable"]
+        puts "Integration for product: #{bundle_name}"
+        puts "=" * 80
+        printf "%-20s: %s\n",                 "id", response["_id"]
+        printf "%-20s: %s\n",            "tiny id", response["tinyID"]
+        printf "%-20s: %s\n",             "bot id", response["bot"]["_id"]
+        printf "%-20s: %s\n",      "current state", response["currentStep"]
+        printf "%-20s: %s\n",             "number", response["number"]
+        printf "%-20s: %s\n",        "queued date", response["queuedDate"]
+        printf "%-20s: %s\n",     "success streak", response["success_streak"]
+        printf "%-20s: %s\n",               "tags", response["tags"].to_s
+        printf "%-20s: %s\n",         "start time", response["startedTime"]
+        printf "%-20s: %s\n",           "end time", response["endedTime"]
+        printf "%-20s: %s\n",           "duration", response["duration"]
+        printf "%-20s: %s\n",      "build summary", response["buildResultSummary"].to_s
+        printf "%-20s: %s\n",             "result", response["result"]
+        printf "%-20s: %s\n",            "product", response["assets"]["product"]["relativePath"]
+        printf "%-20s: %s\n", "source control log", response["assets"]["sourceControlLog"]["relativePath"]
+        printf "%-20s: %s\n",            "archive", response["assets"]["archive"]["relativePath"]
+        printf "%-20s: %s\n",   "xcodebuildOutput", response["assets"]["xcodebuildOutput"]["relativePath"]
+        printf "%-20s: %s\n",          "build log", response["assets"]["xcodebuildLog"]["relativePath"]
+        printf "%-20s: %s\n",  "build service log", response["assets"]["buildServiceLog"]["relativePath"]
+        response["assets"]["triggerAssets"].each do |triggerAsset|
+            printf "%-20s: %s\n",    "trigger log", triggerAsset["relativePath"]
+        end
+    end
+
+    def self.integrate_bot (id)
+        interface = XCodeAPIInterface.instance
+        # get the latest bot configuration
+        response = interface.find_bot id
+        # make a new integration with that config
+        response = interface.create_integration_for_bot id, response
+        puts JSON.pretty_generate response
+    end
+
+    def self.find_bot_id_by_scm_info (name, name_regex)
+        interface = XCodeAPIInterface.instance
+        response = interface.list_bots
+        bot_ids = Array.new
+
+        results = response["results"]
+        results.each do |result|
+            bot_id = result["_id"]
+            response = interface.find_bot bot_id
+            repo_id = response["configuration"]["sourceControlBlueprint"]["DVTSourceControlWorkspaceBlueprintPrimaryRemoteRepositoryKey"]
+            branch_name  = response["configuration"]["sourceControlBlueprint"]["DVTSourceControlWorkspaceBlueprintLocationsKey"][repo_id]["DVTSourceControlBranchIdentifierKey"]
+            project_path = response["configuration"]["sourceControlBlueprint"]["DVTSourceControlWorkspaceBlueprintRelativePathToProjectKey"]
+            if branch_name == name && project_path =~ Regex.new name_regex, Regex::IGNORECASE
+                bot_ids.push bot_id
+            end
+        end
+        return bot_ids
+    end
+
+    def self.find_bot_ids_by_name (name_regex)
+        interface = XCodeAPIInterface.instance
+        response = interface.list_bots
+        bot_ids = Array.new
+
+        results = response["results"]
+        results.each do |result|
+            bot_id = result["_id"]
+            bot_name = result["name"]
+            if bot_name =~ Regex.new name_regex, Regexp::IGNORECASE
+                bot_ids.push bot_id
+            end
+        end
+        return bot_ids
+    end
+end
+
+
+##
+# main entry point
+
+XCodeInteractive.print_bots
+puts
+XCodeInteractive.print_bot_configuration "92a60c96b41506e3528c9945a245ad7c"
+puts
+XCodeInteractive.print_integrations_for_bot "92a60c96b41506e3528c9945a245ad7c"
+puts
+XCodeInteractive.print_integration "92a60c96b41506e3528c9945a245d78d"
+
+puts XCodeInteractive.find_bot_id_by_scm_info "main", "common/AXPlatformTest/AXPlatformTest.xcodeproj"
+
+
+#####################################################################
